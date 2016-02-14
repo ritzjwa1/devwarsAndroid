0f4c884a525ef64d7a92157f1239fd12063f5369
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-02-14 10:28:48 -0500

    Added an activity transition after the profile was changed

--- app/src/main/java/com/example/bukbukbukh/movierating/ProfilePage.java
+++ app/src/main/java/com/example/bukbukbukh/movierating/ProfilePage.java
@@ -1,20 +1,27 @@
 package com.example.bukbukbukh.movierating;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class ProfilePage extends AppCompatActivity {
+
+    String username;
+    String password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
         Intent intent = getIntent();
-        String username = intent.getStringExtra("USER_NAME");
+        username = intent.getStringExtra("USER_NAME");
         Log.d("user", username);
 
     }
@@ -39,5 +46,45 @@ public class ProfilePage extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ChangeProfileTask extends AsyncTask<String, Long, String> {
+        protected String doInBackground(String... urls) {
+            try {
+                HttpRequest request = HttpRequest.post(urls[0]);
+                String result = null;
+                if (request.ok()) {
+                    result = request.body();
+                }
+                return result;
+            } catch (HttpRequest.HttpRequestException exception) {
+                return null;
+            }
+        }
+
+        protected void onProgressUpdate(Long... progress) {
+            //Log.d("MyApp", "Downloaded bytes: " + progress[0]);
+        }
+
+        protected void onPostExecute(String file) {
+            if (file != null) {
+                Intent intent = new Intent(ProfilePage.this, MainMenu.class);
+                startActivity(intent);
+            }
+            else {
+                Log.d("MyApp", "Download failed");
+            }
+        }
+    }
+
+    public void changePassword(View view) {
+        EditText password1 = (EditText) findViewById(R.id.change_password);
+        EditText password2 = (EditText) findViewById(R.id.change_password2);
+        password = password1.getText().toString();
+        if (password.equals(password2.getText().toString())) {
+            new ChangeProfileTask().execute("https://pandango.herokuapp.com/editProfile/" + username + "/" + password);
+        } else {
+
+        }
     }
 }
